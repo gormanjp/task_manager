a8e4a568ef59a85d89cb31855ae1fbd52e56538f
gormanjp <jpgorman8@gmail.com>
2016-08-20 16:55:12 -0400

    added validation for tasks

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,12 +7,12 @@ class ListsController < ApplicationController
 
 	def show
 		@list = List.find_by_id(params[:id])
+		@tasks = @list.tasks
 		@task = Task.new
 	end
 
 	def create
 		@list = List.new(list_params)
-		
 		@list.save
 		@lists = List.all
 		render :index 
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,9 +3,12 @@ class TasksController < ApplicationController
 
   def create
   	@list = List.find(params[:list_id])
-  	@task = @list.tasks.build(task_params)
-  	@task.save
-  	redirect_to list_path(@list)
+  	@task = @list.tasks.new(task_params)
+  	if @task.save
+  		redirect_to task_path(@task)
+  	else
+  		render 'lists/show'
+  	end
   end
 
   def show
