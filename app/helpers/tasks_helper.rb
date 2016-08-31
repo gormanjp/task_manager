be8f314fcc84f496d74553211aab161854a0bd3d
gormanjp <jpgorman8@gmail.com>
2016-08-31 07:50:30 -0400

    added task li helper

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -4,4 +4,11 @@ module TasksHelper
 		"completed" if task.complete?
 	end
 
+	def li_for_task(task)
+		if task.id
+			content_tag_for :li, task, class: li_class_for_task(task) do
+				yield
+			end
+		end
+	end
 end
